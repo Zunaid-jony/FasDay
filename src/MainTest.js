f0324aca0d707c7c565817460725af915ed1677e
Zunaid-jony <jony35-260@diu.edu.bd>
2023-01-03 14:50:27 +0600

    ssss

--- src/MainTest.js
+++ src/MainTest.js
@@ -8,12 +8,14 @@ import Images from './Images'
 import Slider from './Slider'
 import FromSections from './Components/FromSections';
 import TableItem from './Components/TableItems/TableItem'
+import Product from './Components/Product/Product'
 
 const MainTest = () => {
   return (
     <div>
         <Header></Header>
         <TableItem></TableItem>
+        <Product></Product>
         <FromSections></FromSections>
         <Tooltips></Tooltips>
         <ButtonGroub></ButtonGroub>
